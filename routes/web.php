f4954dae83b66f38863de5683a5d55f19bc4b297
fadliikmal <fadliikmal728@gmail.com>
2025-05-30 14:32:34 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\unsurController;
 
+Route::get('/', function () {
+    return view('index');
+})->name('home');
+
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
